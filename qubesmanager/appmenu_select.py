ec39f24711ecd51ecea966463156fb232f3de8eb
Bahtiar `kalkin-` Gadimov <bahtiar@gadimov.de>
2016-07-14 16:38:58 +0200

    qubesmanager.appmenu_select pass flak8

--- qubesmanager/appmenu_select.py
+++ qubesmanager/appmenu_select.py
@@ -16,46 +16,31 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-#
-#
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+# USA.
 
-import sys
 import os
+
+from multiselectwidget import *
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
-
-from qubes.qubes import QubesVmCollection
-from qubes.qubes import QubesException
-from qubes.qubes import QubesDaemonPidfile
-from qubes.qubes import QubesHost
-
-import qubesmanager.resources_rc
-
-from pyinotify import WatchManager, Notifier, ThreadedNotifier, EventsCodes, ProcessEvent
-
-import time
-
-from operator import itemgetter
-
 from thread_monitor import *
-from multiselectwidget import *
 
 whitelisted_filename = 'whitelisted-appmenus.list'
 
+
 class AppListWidgetItem(QListWidgetItem):
-    def __init__(self, name, filename, command, parent = None):
+    def __init__(self, name, filename, command, parent=None):
         super(AppListWidgetItem, self).__init__(name, parent)
         self.setToolTip(command)
         self.filename = filename
 
 
-
 class AppmenuSelectManager:
     def __init__(self, vm, apps_multiselect, parent=None):
 
-        self.app_list = apps_multiselect # this is a multiselect wiget
-        
+        self.app_list = apps_multiselect  # this is a multiselect wiget
+
         self.vm = vm
         if self.vm.template:
             self.source_vm = self.vm.template
@@ -77,7 +62,9 @@ class AppmenuSelectManager:
             f.close()
 
         # Check if appmenu entry is really installed
-        whitelisted = [a for a in whitelisted if os.path.exists('%s/apps/%s-%s' % (self.vm.dir_path, self.vm.name, a))]
+        whitelisted = [a for a in whitelisted
+                       if os.path.exists('%s/apps/%s-%s' % (self.vm.dir_path,
+                                                            self.vm.name, a))]
 
         self.app_list.clear()
 
@@ -94,18 +81,23 @@ class AppmenuSelectManager:
             if not desktop_command:
                 desktop_command = ""
             if desktop_name:
-                available_appmenus.append( (template_file, desktop_name, desktop_command) )
+                available_appmenus.append(
+                    (template_file, desktop_name, desktop_command))
             desktop_template.close()
 
-        self.whitelisted_appmenus = [a for a in available_appmenus if a[0] in whitelisted]
-        available_appmenus = [a for a in available_appmenus if a[0] not in whitelisted]
-                
+        self.whitelisted_appmenus = [a for a in available_appmenus
+                                     if a[0] in whitelisted]
+        available_appmenus = [a for a in available_appmenus
+                              if a[0] not in whitelisted]
+
         for a in available_appmenus:
-            self.app_list.available_list.addItem( AppListWidgetItem(a[1], a[0], a[2]))
+            self.app_list.available_list.addItem(
+                AppListWidgetItem(a[1], a[0], a[2]))
 
         for a in self.whitelisted_appmenus:
-            self.app_list.selected_list.addItem( AppListWidgetItem(a[1], a[0], a[2]))
-   
+            self.app_list.selected_list.addItem(
+                AppListWidgetItem(a[1], a[0], a[2]))
+
         self.app_list.available_list.sortItems()
         self.app_list.selected_list.sortItems()
 
@@ -115,25 +107,26 @@ class AppmenuSelectManager:
 
         for i in range(self.app_list.selected_list.count()):
             item = self.app_list.selected_list.item(i)
-            if item.filename not in [ w[0] for w in self.whitelisted_appmenus]:
+            if item.filename not in [w[0] for w in self.whitelisted_appmenus]:
                 added.append(item)
-        
-        if self.app_list.selected_list.count() - len(added) < len(self.whitelisted_appmenus): #sth removed
+
+        if self.app_list.selected_list.count() - len(added) < \
+                len(self.whitelisted_appmenus):  # sth removed
             sth_changed = True
         elif len(added) > 0:
             sth_changed = True
-        
-        if sth_changed == True:
-            whitelisted = open(self.vm.dir_path + '/' + whitelisted_filename, 'w')
+
+        if sth_changed:
+            whitelisted = open(self.vm.dir_path + '/' + whitelisted_filename,
+                               'w')
             for i in range(self.app_list.selected_list.count()):
                 item = self.app_list.selected_list.item(i)
                 whitelisted.write(item.filename + '\n')
-            whitelisted.close() 
+            whitelisted.close()
             return True
         else:
             return False
-               
- 
+
     def save_appmenu_select_changes(self):
         if self.save_list_of_selected():
             self.vm.appmenus_recreate()
